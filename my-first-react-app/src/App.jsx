6913022a04b2dae63c11d829a5b0fb419353eb72
Nikhil Kalhan <84812103+nikhilkalhan92@users.noreply.github.com>
2025-03-28 23:54:35 +0000

    added css for the titles on the app.js, also css for the card title

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,17 +1,16 @@
 const Card = ({ title }) => {
   return (
-    <div>
+    <div className="card">
       <h2>{title}</h2>
-    </div>
+      </div>
+
   )
 }
 
 const App = () => {
   return (
-    <div>
-    <h2>Functional Arrows</h2>
-
-    <Card title="Star Wars" />
+    <div className="card-container">
+    <Card title="Star Wars" rating={5} isCool={true} />
     <Card title="Avatar" />
     <Card title="Lion King" />
     </div>
