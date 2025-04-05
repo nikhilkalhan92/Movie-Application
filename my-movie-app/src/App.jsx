8a3700bab498c3573f4ffae4c3f3e6fc5930213f
Nikhil Kalhan <84812103+nikhilkalhan92@users.noreply.github.com>
2025-04-05 23:30:13 +0000

    installed vite, react with js, tailwind css, added some assets (pictures)

---
+++ my-movie-app/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div className='pattern' />
+
+      <div className='wrapper'>
+      <header>
+        <h1>Find <span className='text-gradient'>Movies </span>You'll enjoy without the hassle</h1>
+      </header>
+
+      
+      </div>
+    </main>
+  )
+}
+
+export default App
